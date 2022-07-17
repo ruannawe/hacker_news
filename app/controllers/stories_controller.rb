71e95705d5f4c2837d8f68cb2c1273d25a37fa9e
Ruan Herculano <ruannawe95@outlook.com>
2022-07-17 06:33:38 -0300

    Downgrade to ruby 3.1.2

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,6 @@
 class StoriesController < ApplicationController
   def index
-    @items = Item.where(item_type: :story).limit(10).order(created_at: :desc)
+    @items = Item.where(item_type: :story).order(created_at: :desc).limit(10)
   end
 
   def show
--- app/sidekiq/hacker_news_job.rb
+++ app/sidekiq/hacker_news_job.rb
@@ -2,7 +2,6 @@ class HackerNewsJob
   include Sidekiq::Job
 
   def perform(*args)
-    puts 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
     HackerNewsService.new.top_stories
   end
 end
