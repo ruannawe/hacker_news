f3e2c0b4899290bba54d209c0e6f370c3d9fae02
Ruan Herculano <ruannawe95@outlook.com>
2022-07-17 05:30:32 -0300

    Helper to count comments

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -2,4 +2,15 @@ module StoriesHelper
   def format_link(link)
     URI.parse(link).host
   end
+
+  def count_comments(items, amount = 0)
+    return amount if items.empty?
+
+    items.each do |item|
+      amount += 1
+      amount = count_comments(item.items, amount)
+    end
+
+    amount
+  end
 end
